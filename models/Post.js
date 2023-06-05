7b5bc3dae6d932ff057d17722772a62ad4ef80cb
Misha Panivnyk <panivnykm@gmail.com>
2023-06-05 17:46:04 +0300

    add models/Post

---
+++ models/Post.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String, required: true },
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  { versionKey: false }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
